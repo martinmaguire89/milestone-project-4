6cfb96fc42d2ed3d70e53945d860b6997d5c8acc
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-04-27 15:04:30 +0000

    cart app created with session incside of context.py to allow user to store and look at items when logged in

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+""" This will display the products on thr poduct page and datebase"""
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
