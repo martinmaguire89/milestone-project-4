d24554038500dcbc408a7bdd8ee61357344e5e34
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-05-21 15:08:04 +0000

    views.py created for post app

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import BlogPostForm
 
-# Create your views here.
+
+def get_posts(request):
+    """
+    This will show a list of all blogs
+    published on post.html template
+    """
+
+    posts = Post.objects.filter(published_date__lte=timezone.now
+                                ()).order_by('-published_date')
+    return render(request, "posts.html", {'posts': posts})
+
+
+def post_detail(request, pk):
+    """
+    Create a view that returns a single
+    Post object based on the post ID (pk) and
+    render it to the 'postdetail.html' template.
+    Or return a 404 error if the post is
+    not found
+    """
+
+    post = get_object_or_404(Post, pk=pk)
+    post.views += 1
+    post.save()
+    return render(request, 'postdetail.html', {'post': post})
+
+
+def create_or_edit_post(request, pk=None):
+    """
+     view that allows us to create
+    or edit a post depending if the Post ID
+    is null
+    """
+
+    post = get_object_or_404(Post, pk=pk) if pk else None
+    if request.method == "post":
+        form = BlogPostForm(request.POST, request.Files, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post_detail, post.pk)
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'postform.html', {'form': form})
