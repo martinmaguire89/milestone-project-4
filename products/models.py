7f7d5cd1e65657a5bc1cad59bb14dce5710ec347
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-06-09 10:54:58 +0000

    git choices added to provide a dropdown in the product database to allow filtering with them

--- products/models.py
+++ products/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 """ This will display the products on the poduct page and datebase"""
 
+PRODUCT_CHOICES = ([
+        ('singles', 'singles'),
+        ('mixed_cases', 'mixed_cases'),
+        ('merchandise', 'merchandise'),
+    ])
 
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+    categories = models.CharField(max_length=120, choices=PRODUCT_CHOICES,
+                                  default='singles')
 
     def __str__(self):
         return self.name
