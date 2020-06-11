c2a626b263e2400708e03981490e6b446e792a1a
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-06-11 12:59:30 +0000

    code removed from products models and views as no longer needed

--- products/models.py
+++ products/models.py
@@ -8,21 +8,13 @@ PRODUCT_CHOICES = ([
         ('merchandise', 'merchandise'),
     ])
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=120, choices=PRODUCT_CHOICES,
-                            default='singles')
-
-    def __str__(self):
-        return self.name
-
-
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
-    categories = models.ManyToManyField(Category)
+    categories = models.CharField(max_length=120, choices=PRODUCT_CHOICES,
+                                  default='singles')
 
     def __str__(self):
         return self.name
