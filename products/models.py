0689a3de928bca7e0806c60a90903f3cb8bd897a
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-06-11 07:25:08 +0000

    models.py in product update to add a category to filter

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-""" This will display the products on the poduct page and datebase"""
+""" This will display the products on the poduct page and database"""
 
 PRODUCT_CHOICES = ([
         ('singles', 'singles'),
@@ -8,13 +8,21 @@ PRODUCT_CHOICES = ([
         ('merchandise', 'merchandise'),
     ])
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=120, choices=PRODUCT_CHOICES,
+                            default='singles')
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
-    categories = models.CharField(max_length=120, choices=PRODUCT_CHOICES,
-                                  default='singles')
+    categories = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.name
